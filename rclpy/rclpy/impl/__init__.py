c3a396fe88d47a733f19ea1485ef2d4a4692974d
Michael Carroll <michael@openrobotics.org>
2020-12-04 16:02:32 -0600

    Rebase from 'upstream'

--- rclpy/rclpy/impl/__init__.py
+++ rclpy/rclpy/impl/__init__.py
@@ -14,16 +14,28 @@
 
 import importlib
 import os
+from pathlib import Path
 
 
 def _import(name):
     try:
         return importlib.import_module(name, package='rclpy')
     except ImportError as e:
+        if e.path is None:
+            import sysconfig
+            expected_path = Path(__file__).parents[1] / (
+                name[1:] + sysconfig.get_config_var('EXT_SUFFIX'))
+            assert not expected_path.is_file()
+            e.msg += \
+                f"\nThe C extension '{expected_path}' isn't present on the " \
+                "system. Please refer to 'https://index.ros.org/doc/ros2/" \
+                'Troubleshooting/Installation-Troubleshooting/#import-' \
+                "failing-without-library-present-on-the-system' for " \
+                'possible solutions'
         if e.path is not None and os.path.isfile(e.path):
             e.msg += \
                 "\nThe C extension '%s' failed to be imported while being present on the system." \
                 " Please refer to '%s' for possible solutions" % \
-                (e.path, 'https://index.ros.org/doc/ros2/Troubleshooting/'
+                (e.path, 'https://index.ros.org/doc/ros2/Troubleshooting/Installation-Troubleshooting/'
                          '#import-failing-even-with-library-present-on-the-system')
         raise
