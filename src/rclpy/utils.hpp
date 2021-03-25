1c45fe0cc8ef77cd05f4ed4469cbd135476f7dd1
Chris Lalancette <clalancette@openrobotics.org>
2021-03-25 21:12:31 +0000

    Rebase from 'rpm/rolling/rclpy'

--- src/rclpy/utils.hpp
+++ src/rclpy/utils.hpp
@@ -12,19 +12,21 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-#ifndef RCLPY_COMMON__COMMON_HPP_
-#define RCLPY_COMMON__COMMON_HPP_
+#ifndef RCLPY__UTILS_HPP_
+#define RCLPY__UTILS_HPP_
 
 #include <pybind11/pybind11.h>
 
 #include <rcl/graph.h>  // rcl_names_and_types_t
 
-#include "rclpy_common/visibility_control.h"
+#include <memory>
 
 namespace py = pybind11;
 
 namespace rclpy
 {
+
+typedef void destroy_ros_message_function (void *);
 
 /// Convert a C rcl_names_and_types_t into a Python list.
 /**
@@ -32,10 +34,30 @@ namespace rclpy
  * \return List of tuples, where the first element of each tuple is a string
  *   for the name and the second element is a list of strings for the types.
  */
-RCLPY_COMMON_PUBLIC
 py::list
 convert_to_py_names_and_types(const rcl_names_and_types_t * topic_names_and_types);
 
+/// Create the equivalent ROS message C type instance for a given Python type.
+/**
+* Raises AttributeError if \p pyclass is missing a required attribute.
+*
+* \param[in] pyclass ROS message Python type to extract typesupport data from.
+* \return a ROS message C type instance.
+*/
+std::unique_ptr<void, destroy_ros_message_function *>
+create_from_py(py::object pyclass);
+
+/// Convert a ROS message from a C type to a Python type.
+/**
+ * Raises AttributeError if \p pyclass is missing a required attribute.
+ *
+ * \param[in] message ROS message C type instance to be converted.
+ * \param[in] pyclass ROS message Python type to convert to.
+ * \return an instance of \p pyclass.
+ */
+py::object
+convert_to_py(void * message, py::object pyclass);
+
 }  // namespace rclpy
 
-#endif  // RCLPY_COMMON__COMMON_HPP_
+#endif  // RCLPY__UTILS_HPP_
