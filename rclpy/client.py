13e1eef1cb6b82a7036c2bc4709e996b35064e2d
Chris Lalancette <clalancette@gmail.com>
2023-09-07 14:57:41 +0000

    Rebase from 'debian/rolling/rclpy'

--- rclpy/client.py
+++ rclpy/client.py
@@ -45,7 +45,7 @@ class Client:
         """
         Create a container for a ROS service client.
 
-        .. warning:: Users should not create a service client with this constuctor, instead they
+        .. warning:: Users should not create a service client with this constructor, instead they
            should call :meth:`.Node.create_client`.
 
         :param context: The context associated with the service client.
@@ -203,6 +203,11 @@ class Client:
     @property
     def handle(self):
         return self.__client
+
+    @property
+    def service_name(self) -> str:
+        with self.handle:
+            return self.__client.service_name
 
     def destroy(self):
         self.__client.destroy_when_not_in_use()
