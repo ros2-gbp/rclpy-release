346dcdda646384961ed6b7577669f0d803d70fc1
Alejandro Hernandez Cordero <ahcorde@gmail.com>
2025-10-21 12:23:20 +0200

    Rebase from 'release/kilted/rclpy'

--- rclpy/node.py
+++ rclpy/node.py
@@ -83,6 +83,7 @@ from rclpy.qos_overriding_options import QoSOverridingOptions
 from rclpy.service import Service
 from rclpy.subscription import MessageInfo
 from rclpy.subscription import Subscription
+from rclpy.subscription_content_filter_options import ContentFilterOptions
 from rclpy.time_source import TimeSource
 from rclpy.timer import Rate
 from rclpy.timer import Timer, TimerInfo
@@ -550,7 +551,6 @@ class Node:
                 )
 
             value = None
-            param_type = None
 
             # Get the values from the tuple, checking its types.
             # Use defaults if the tuple doesn't contain value and / or descriptor.
@@ -1695,7 +1695,8 @@ class Node:
         callback_group: Optional[CallbackGroup] = None,
         event_callbacks: Optional[SubscriptionEventCallbacks] = None,
         qos_overriding_options: Optional[QoSOverridingOptions] = None,
-        raw: bool = False
+        raw: bool = False,
+        content_filter_options: Optional[ContentFilterOptions] = None
     ) -> Subscription[MsgT]:
         """
         Create a new subscription.
@@ -1713,6 +1714,7 @@ class Node:
         :param event_callbacks: User-defined callbacks for middleware events.
         :param raw: If ``True``, then received messages will be stored in raw binary
             representation.
+        :param content_filter_options: The filter expression and parameters for content filtering.
         """
         qos_profile = self._validate_qos_or_depth_parameter(qos_profile)
 
@@ -1740,7 +1742,8 @@ class Node:
         try:
             with self.handle:
                 subscription_object = _rclpy.Subscription(
-                    self.handle, msg_type, topic, qos_profile.get_c_qos_profile())
+                    self.handle, msg_type, topic, qos_profile.get_c_qos_profile(),
+                    content_filter_options)
         except ValueError:
             failed = True
         if failed:
